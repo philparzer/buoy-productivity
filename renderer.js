de5c38f3590aae81d49256d3a9e06fc0bb1051cb
Philipp Parzer <phil0898.pp@gmail.com>
2021-05-09 05:46:42 +0200

    implemented close and minimize buttons

--- renderer.js
+++ renderer.js
@@ -1,6 +1,19 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+//title bar
+
+const { ipcRenderer } = require( 'electron' );
+
+document.getElementById('close-main').onclick = function() {
+    ipcRenderer.send( 'app:quit' );
+}
+
+document.getElementById('minimize-main').onclick = function() {
+    ipcRenderer.send( 'app:minimize' );
+}
+
+ipcMain.on( 'app:minimize', () => {
+    remote.BrowserWindow.getFocusedWindow().minimize();
+  } )
+
+//TODO: Timer, other buttons, writing to file
+
+//workers for timer?
