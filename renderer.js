c64d3f1f8ace7aa56dd12ea977c2d56cb86a21bd
johannes-p <83518424+johannes-p@users.noreply.github.com>
2021-05-10 02:32:36 +0200

    timer logic added

--- renderer.js
+++ renderer.js
@@ -16,4 +16,24 @@ ipcMain.on( 'app:minimize', () => {
 
 //TODO: Timer, other buttons, writing to file
 
+//--Timer--
+
+document.getElementById('start-box').onclick = function(){
+    
+    console.log("TEST")
+
+    let timer = parseInt(document.getElementById('minutes').nodeValue()) * 1000 /* Seconds to Mins -->  x60 */;
+    let start = Date.now();
+    
+    
+    setInterval(function() {
+        var delta = Date.now() - start;
+        
+        if(delta >= timer){
+            alert("Time over");
+        }
+        }, 1000);
+}
+//---------
+
 //workers for timer?
