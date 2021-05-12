76d32da63ee17495b03ed099f43fe53d4a3d29ae
Philipp Parzer <phil0898.pp@gmail.com>
2021-05-12 05:24:33 +0200

    frontend stuff, laid foundation for focus check

--- renderer.js
+++ renderer.js
@@ -1,12 +1,12 @@
-//VARS ------------------------------------------------------------
+//VARS  ########################################################################################################################
 
-//requires
+//imports --------------------------------------------------------------------------------------------
 const { ipcRenderer } = require('electron');
 var sqlite3 = require('sqlite3').verbose(); //also const?
 const { desktopCapturer } = require('electron')
 const activeWindows = require('electron-active-window');
 
-//colors
+//colors  --------------------------------------------------------------------------------------------
 const red = '#DBA993';
 const yellow =  "#DBDB93";
 const white =  "#FAFAFA";
@@ -14,7 +14,7 @@ const primaryGrey = "#474747";
 const darkerGrey =  "#3F3F3F";
 const lighterGrey =  "#707070";
 
-//buttons
+//buttons  --------------------------------------------------------------------------------------------
 const aboutBtn = document.getElementById('about');
 const settingsBtn = document.getElementById('settings');
 
@@ -23,12 +23,11 @@ const addMinutesBtn = document.getElementById('time-minutes-add-btn');
 const subtractHoursBtn = document.getElementById('time-hours-subtract-btn');
 const subtractMinutesBtn = document.getElementById('time-minutes-subtract-btn');
 
-//const tagBtn;
-const focusBtn = document.getElementById('focus-btn')
+const focusBtn = document.getElementById('focus-btn');
 const startBtn = document.getElementById('start-btn');
 
 
-//time
+//time  --------------------------------------------------------------------------------------------
     //main time calculation variables
 let mins = 30; 
 let hours = 0;
@@ -39,31 +38,10 @@ let hrsInput;
 const hoursElement = document.getElementById('hours');
 const minutesElement = document.getElementById('minutes');
 
+//focus  --------------------------------------------------------------------------------------------
+let focusSet = false;
 
-//REUSABLE FUNCTIONS   ------------------------------------------------------------
-
-function switchButtonStatus(){
-    startBtn.disabled = !startBtn.disabled;
-    addHoursBtn.disabled = !addHoursBtn.disabled;
-    addMinutesBtn.disabled = !addMinutesBtn.disabled;
-    subtractHoursBtn.disabled = !subtractHoursBtn.disabled;
-    subtractMinutesBtn.disabled = !subtractMinutesBtn.disabled;
-}
-
-    //formats numbers in timer to contain 0 at beginning
-function numberFormatter(number){
-
-    if(number.toString().length < 2){
-        number = '0' + number;
-    }
-
-    return number;
-
-} 
-
-//TITLEBAR  ------------------------------------------------------------
-
-
+//TITLEBAR  ########################################################################################################################
 
 document.getElementById('close-main').onclick = function() {
     ipcRenderer.send( 'app:quit' );
@@ -73,7 +51,7 @@ document.getElementById('minimize-main').onclick = function() {
     ipcRenderer.send( 'app:minimize' );
 }
 
-//DATABASE  ------------------------------------------------------------
+//DATABASE  ########################################################################################################################
 
 function databaseWrite(valueArray){
     var db = new sqlite3.Database('./buoy-db.db');
@@ -83,27 +61,29 @@ function databaseWrite(valueArray){
 
 databaseWrite(["TEST","CREATIVE","mins", 0,"DATUM"])
 
-//TIMER - MAIN  ------------------------------------------------------------
+//MAIN  ########################################################################################################################
+
+if (focusSet == false){ //make sure that focus is set before enabling start button
+    disableStartBtn();
+}
 
 startBtn.onclick = function(){ //starts the timer
-
     setInputTimes(); //sets the fixed input values for the count variable
     switchButtonStatus(); //switches buttons to unclickable
     styleBuoy(); //styles buoy to indicate that timer is running
-
-    
+    styleBackground();
     let timerInput = (parseInt(hoursElement.textContent) * 1000 * 60 * 60) + (parseInt(minutesElement.textContent) * 1000 * 60); //let timerInput: time input by user parsed from HTML elements and converted into milliseconds
     let startingTime = Date.now(); //let starting time = current system clock local time
     
+    //TODO: focus set
+
     
-    //main timer functionality
+    //timer functionality  --------------------------------------------------------------------------------------------
     let timerLogic =  setInterval(function() {
         var delta = Date.now() - startingTime; //delta is time difference from start in ms
         
         //counter
         var count = ((hrsInput*60) + minsInput) - (delta / 1000 / 60); //count takes initial user input values and calculates time passed in float minutes
-
-        
         if(count < (hours*60 + mins) -1 )
 
             //minutes html update
@@ -119,28 +99,37 @@ startBtn.onclick = function(){ //starts the timer
                 minutesElement.textContent = numberFormatter(59);
             }
         
+        //TODO: focus check 
         
+
         //timer finished
         if(delta >= timerInput) { 
             switchButtonStatus();
             unstyleBuoy();
-            alert("Time Over"); //TODO: PLACEHOLDER ALERT
+            unstyleBackground();
+            alert("Time Over");
             clearInterval(timerLogic);
             minutesElement.textContent = numberFormatter(0);
-            hoursElement.textContent = numberFormatter(0); 
+            hoursElement.textContent = numberFormatter(0);
+            focusSet = false;
+            disableStartBtn();
         }
-        }, 1000); //<- interval of 1second 
+        }, 1000);
 }
 
-//TIMER - Input  ------------------------------------------------------------
 
-    //onclick Events
+
+
+
+//TIMER - Input  ########################################################################################################################
+
+    //onclick Events  --------------------------------------------------------------------------------------------
 addMinutesBtn.onclick = function(){TimerInput(addMinutesBtn);}
 addHoursBtn.onclick = function(){TimerInput(addHoursBtn);}
 subtractMinutesBtn.onclick = function(){TimerInput(subtractMinutesBtn);}
 subtractHoursBtn.onclick = function(){TimerInput(subtractHoursBtn);}
     
-    //button functionality and calls to sanitycheck input
+    //button functionality and calls to sanitycheck input  --------------------------------------------------------------------------------------------
 function TimerInput(btn) {
     if (btn == addMinutesBtn){
         mins++;
@@ -159,53 +148,24 @@ function TimerInput(btn) {
         hoursSanityCheck();
     }
 
-    //update HTML afterwards
+    //update HTML  --------------------------------------------------------------------------------------------
     minutesElement.textContent = numberFormatter(mins);
     hoursElement.textContent = numberFormatter(hours);
 };
 
-    //sets input times once on start click
-function setInputTimes(){
-    hrsInput = hours;
-    minsInput = mins;
-}
-
-    //Input Sanity Checks
-
-function hoursSanityCheck(){
-    
-    if (hours == -1)
-    {
-        hours = 99;
-    }
-
-    else if (hours >= 99)
-    {
-        hours = 0; //changed from 1 to 0
-    }
-
-}
-
-function minuteSanityCheck(){
-    if (mins == -1){
-        mins = 59;
-    }
-    
-    if (mins == 60){
-        mins = 0;
-    }
-}
 
 
-//FOCUS  ------------------------------------------------------------
+//FOCUS  ########################################################################################################################
 
 
 
 focusBtn.onclick = function(){
     //set focus app and exceptions
+    focusSet = true;
+    enableStartBtn();
 }
 
-    //TODO: check set focus app and exceptions
+//TODO: check set focus app and exceptions
     /*activeWindows().getActiveWindow().then((result)=>{
         console.log(result)
     });*/
@@ -216,7 +176,93 @@ focusBtn.onclick = function(){
 
 
 
-//Buoy Styling  ------------------------------------------------------------
+//UTIL FUNCTIONS   ########################################################################################################################
+
+function switchButtonStatus(){
+    startBtn.disabled = !startBtn.disabled;
+    addHoursBtn.disabled = !addHoursBtn.disabled;
+    addMinutesBtn.disabled = !addMinutesBtn.disabled;
+    subtractHoursBtn.disabled = !subtractHoursBtn.disabled;
+    subtractMinutesBtn.disabled = !subtractMinutesBtn.disabled;
+    focusBtn.disabled = !focusBtn.disabled;
+}
+
+
+function disableStartBtn(){
+    startBtn.disabled = true;
+    document.getElementById('Rectangle_13').style.display = "none";
+    document.getElementById('start').textContent ="";
+}
+
+function enableStartBtn(){
+    document.getElementById('Rectangle_13').style.display = "unset";
+    document.getElementById('start').textContent = "start";
+    startBtn.disabled = false;
+}
+
+    //formats numbers in timer to contain 0 at beginning
+function numberFormatter(number){
+
+    if(number.toString().length < 2){
+        number = '0' + number;
+    }
+
+    return number;
+
+}
+
+    //sets input times once on start click
+function setInputTimes(){
+    hrsInput = hours;
+    minsInput = mins;
+}
+
+    //Input Sanity Checks
+
+    function hoursSanityCheck(){
+    
+        if (hours == -1)
+        {
+            hours = 99;
+        }
+    
+        else if (hours >= 99)
+        {
+            hours = 0; //changed from 1 to 0
+        }
+    
+    }
+    
+    function minuteSanityCheck(){
+        if (mins == -1){
+            mins = 59;
+        }
+        
+        if (mins == 60){
+            mins = 0;
+        }
+    }
+    
+
+
+//Styling  ########################################################################################################################
+
+function styleBackground(){
+    //  dots fade out
+
+    try{document.getElementById("fade-out-bg").id = "main-background"} //ensures that bg fade works second time around
+    catch{console.log("startup")};
+
+
+    document.getElementById("dot-1").className = "fade-out";
+    document.getElementById("dot-2").className = "fade-out";
+    document.getElementById("dot-3").className = "fade-out";
+    document.getElementById("dot-4").className = "fade-out";
+    document.getElementById("dot-5").className = "fade-out";
+
+    //background image
+    document.getElementById("main-background").id = "fade-in-bg";
+}
 
 function styleBuoy(){
     document.getElementById('semicolon').style.color = red;
@@ -225,16 +271,19 @@ function styleBuoy(){
     document.getElementById('hrs-btn-down').style.display = 'none';
     document.getElementById('min-btn-down').style.display = 'none';
 
+    //focus buttons
+    document.getElementById('Ellipse_8').style.fill = red;
+    document.getElementById('focus').style.fill = darkerGrey;
+
     //tag button
     document.getElementById('Rectangle_15').style.fill = red;
 
     //start button
-    document.getElementById('Rectangle_13').style.fill = red;
-    document.getElementById('start').textContent = "running";
-    document.getElementById('start').style.fill = darkerGrey;
-    document.getElementById('start').style.transform = "translate(21px, 21px)";
+    document.getElementById('start-box').style.display = 'none';
+    document.getElementById('loadingButton').style.display = 'unset';
+    document.getElementById('focus-btn').style.marginBottom ='-70px';
 
-
+    
 }
 
 function unstyleBuoy(){
@@ -244,12 +293,29 @@ function unstyleBuoy(){
     document.getElementById('hrs-btn-down').style.display = 'unset';
     document.getElementById('min-btn-down').style.display = 'unset';
 
+    //focus buttons
+    document.getElementById('Ellipse_8').style.fill = primaryGrey;
+    document.getElementById('focus').style.fill = white;
+
     //tag button
     document.getElementById('Rectangle_15').style.fill = yellow;
 
     //start button
-    document.getElementById('Rectangle_13').style.fill ="";
-    document.getElementById('start').textContent = "start";
-    document.getElementById('start').style.fill = "";
-    document.getElementById('start').style.transform = "translate(32px, 21px)";
+    document.getElementById('loadingButton').style.display ='none';
+    document.getElementById('start-box').style.display = 'unset';
+    document.getElementById('focus-btn').style.marginBottom ='unset';
+}
+
+function unstyleBackground(){
+
+    //  dots fade in
+    document.getElementById("dot-1").className = "fade-in";
+    document.getElementById("dot-2").className = "fade-in";
+    document.getElementById("dot-3").className = "fade-in";
+    document.getElementById("dot-4").className = "fade-in";
+    document.getElementById("dot-5").className = "fade-in";
+    
+    //background image fade in
+    document.getElementById("fade-in-bg").id = "fade-out-bg";
+    
 }
