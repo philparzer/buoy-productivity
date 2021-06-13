f3451261d1bb0f393fcbb38f86f71eed63a0aa40
Philipp Parzer <phil0898.pp@gmail.com>
2021-06-13 15:44:38 +0200

    updated styling, disabled ctrl+r / cmd+r -> mac needs testing

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-// Modules to control application life and create native browser window
 const {app, BrowserWindow, ipcMain, remote} = require('electron')
 const path = require('path')
 var sqlite3 = require('sqlite3').verbose(); 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
-// let splash; TODO: think about SPLASH SCREEN
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 let db = new sqlite3.Database('./buoy.db', (err) => {
   if (err) {
      console.error(err.message);
      //TODO: if missing database file -> Display database file downloadlink (Hide Inputs/Stop Programm?)
   } else{ console.log("Connected")}});
 
-//TODO: disable tab here?
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function createWindow () {
   // Create the browser window.
   
@@ -23,28 +22,61 @@ function createWindow () {
     webPreferences: {nodeIntegration: true, contextIsolation: false, spellcheck: false}
   })
 
-  if (process.platform !== 'darwin'){mainWindow.setIcon(path.join(__dirname, '/images/icon.ico'));} //icon.ico throws error on mac
+  //WINDOWS
+  if (process.platform !== 'darwin')
+  {
+    mainWindow.setIcon(path.join(__dirname, '/images/icon.ico'));
+    mainWindow.removeMenu() //disables ctrl r
+  }
   
+  //MACOS
+  else
+  {
+    //TODO: handle icon
+    Menu.setApplicationMenu(Menu.buildFromTemplate([])); //disables ctrl r
+  }
 
-  // and load the index.html of the app.
-  //mainWindow.loadFile('index.html');
+  //load window
   DBGetSettingsLanguage(mainWindow)
-  
-
-//TODO: think about SPLASH SCREEN           // create a new `splash`-Window  
-                                            // splash = new BrowserWindow({width: 810, height: 610, transparent: true, frame: false, alwaysOnTop: true});
-                                            // splash.loadURL(`file://${__dirname}/splash.html`);
-                                            // mainWindow.loadURL(`file://${__dirname}/index.html`);
-                                          
-                                            // // if main window is ready to show, then destroy the splash window and show up the main window
-                                            // mainWindow.once('ready-to-show', () => {
-                                            //   splash.destroy();
-                                            //   mainWindow.show();
-                                            // });
-
-  // Open the DevTools.
+  // Open DevTools.
   mainWindow.webContents.openDevTools()
 }
+
+function DBGetSettingsLanguage(mainWindow){ 
+
+  db.get('SELECT language FROM settings WHERE ROWID = 1', (error, row) => {
+    
+    switch(row.language){
+      case "en":
+        console.log("en selected")
+        mainWindow.loadFile('index.html');
+        break;
+
+      case "ru":
+        console.log("ru selected")
+        mainWindow.loadFile('index-ru.html');
+        break;
+
+      case "de":
+        console.log("de selected")
+        mainWindow.loadFile('index-de.html');
+        break;
+
+      case "fr":
+        console.log("fr selected")
+        mainWindow.loadFile('index-fr.html');
+        break;
+
+      default:
+        console.log("language not specified");
+
+    }
+    
+  });
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -86,38 +118,3 @@ ipcMain.on( 'app:minimize', () => {
 } )
 
 
-function DBGetSettingsLanguage(mainWindow){ //TODO: default radio button value to html file language (set in html doc)
-  //TODO: Establish db connection in main.js
-  db.get('SELECT language FROM settings WHERE ROWID = 1', (error, row) => {
-    
-    switch(row.language){
-      case "en":
-        console.log("en selected")
-        mainWindow.loadFile('index.html');
-        break;
-
-      case "ru":
-        console.log("ru selected")
-        mainWindow.loadFile('index-ru.html');
-        break;
-
-      case "de":
-        console.log("de selected")
-        mainWindow.loadFile('index-de.html');
-        break;
-
-      case "fr":
-        console.log("fr selected")
-        mainWindow.loadFile('index-fr.html');
-        break;
-
-      default:
-        console.log("language not specified")
-        //mainWindow.loadFile('index.html');
-
-    }
-    
-  });
-
-  //TODO: load html file with specified language
-}
